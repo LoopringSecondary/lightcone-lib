3d01d725c7af777cfcb0ce14145033eb2e7a337b
dylen <dylenfu@126.com>
2018-11-16 16:59:24 +0800

    modify order generate hash

--- src/main/scala/org/loopring/lightcone/lib/base/Order.scala
+++ src/main/scala/org/loopring/lightcone/lib/base/Order.scala
@@ -50,90 +50,74 @@ case class Order(
     brokerSpendableFee: BigInt = 0,
 ) {
 
-  val eip712Open = true
+  /*
+  * // Precalculated EIP712_ORDER_SCHEMA_HASH amd EIP712_DOMAIN_HASH because
+        // the solidity compiler doesn't correctly precalculate them for us.
+        bytes32 _EIP712_ORDER_SCHEMA_HASH = 0x5632ff1bdfbe9ca7ecbcb1bd8c61f364e0debfed45fd8be4e459081586292fff;
+        bytes32 _EIP712_DOMAIN_HASH = 0xaea25658c273c666156bd427f83a666135fcde6887a6c25fc1cd1562bc4f3f34;
 
+        bytes32 hash;
+        assembly {
+            let ptr := mload(64)
+            mstore(add(ptr,   0), _EIP712_ORDER_SCHEMA_HASH)     // EIP712_ORDER_SCHEMA_HASH
+            mstore(add(ptr,  32), mload(add(order, 128)))        // order.amountS
+            mstore(add(ptr,  64), mload(add(order, 160)))        // order.amountB
+            mstore(add(ptr,  96), mload(add(order, 640)))        // order.feeAmount
+            mstore(add(ptr, 128), mload(add(order, 192)))        // order.validSince
+            mstore(add(ptr, 160), mload(add(order, 480)))        // order.validUntil
+            mstore(add(ptr, 192), mload(add(order,  32)))        // order.owner
+            mstore(add(ptr, 224), mload(add(order,  64)))        // order.tokenS
+            mstore(add(ptr, 256), mload(add(order,  96)))        // order.tokenB
+            mstore(add(ptr, 288), mload(add(order, 288)))        // order.dualAuthAddr
+            mstore(add(ptr, 320), mload(add(order, 320)))        // order.broker
+            mstore(add(ptr, 352), mload(add(order, 416)))        // order.orderInterceptor
+            mstore(add(ptr, 384), mload(add(order, 448)))        // order.wallet
+            mstore(add(ptr, 416), mload(add(order, 768)))        // order.tokenRecipient
+            mstore(add(ptr, 448), mload(add(order, 608)))        // order.feeToken
+            mstore(add(ptr, 480), mload(add(order, 800)))        // order.walletSplitPercentage
+            mstore(add(ptr, 512), mload(add(order, 704)))        // order.tokenSFeePercentage
+            mstore(add(ptr, 544), mload(add(order, 736)))        // order.tokenBFeePercentage
+            mstore(add(ptr, 576), mload(add(order, 576)))        // order.allOrNone
+            let message := keccak256(ptr, 608)                   // 19 * 32
+
+            mstore(add(ptr,  0), 0x1901)                         // EIP191_HEADER
+            mstore(add(ptr, 32), _EIP712_DOMAIN_HASH)            // EIP712_DOMAIN_HASH
+            mstore(add(ptr, 64), message)                        // message
+            hash := keccak256(add(ptr, 30), 66)                  // 2 + 32 + 32
+  * */
   def generateHash: String = {
-    if (eip712Open) {
-      val typeData = toTypeData()
-      typeData.signatureHash()
-    } else {
-      val data = ByteStream()
-      data.addUint(amountS)
-      data.addUint(amountB)
-      data.addUint(feeAmount)
-      data.addUint(BigInt(validSince))
-      data.addUint(BigInt(validUntil))
-      data.addAddress(owner, true)
-      data.addAddress(tokenS, true)
-      data.addAddress(tokenB, true)
-      data.addAddress(dualAuthAddress, true)
-      data.addAddress(broker,true)
-      data.addAddress(orderInterceptor, true)
-      data.addAddress(wallet, true)
-      data.addAddress(tokenReceipt, true)
-      data.addAddress(feeToken, true)
-      data.addUint16(walletSplitPercentage)
-      data.addUint16(tokenSFeePercentage)
-      data.addUint16(tokenBFeePercentage)
-      data.addBoolean(allOrNone)
+    val EIP712_HEADER = "0x1901"
+    val EIP712_ORDER_SCHEMA_HASH = "0x5632ff1bdfbe9ca7ecbcb1bd8c61f364e0debfed45fd8be4e459081586292fff"
+    val EIP712_DOMAIN_HASH = "0xaea25658c273c666156bd427f83a666135fcde6887a6c25fc1cd1562bc4f3f34"
 
-      Numeric.toHexString(web3Hash.sha3(data.getBytes))
-    }
-  }
+    val data = ByteStream()
+    data.addPadHex(EIP712_ORDER_SCHEMA_HASH)
+    data.addUint(amountS)
+    data.addUint(amountB)
+    data.addUint(feeAmount)
+    data.addUint(BigInt(validSince))
+    data.addUint(BigInt(validUntil))
+    data.addAddress(owner, 32, true)
+    data.addAddress(tokenS, 32,true)
+    data.addAddress(tokenB, 32,true)
+    data.addAddress(dualAuthAddress, 32, true)
+    data.addAddress(broker,32,true)
+    data.addAddress(orderInterceptor, 32, true)
+    data.addAddress(wallet, 32,true)
+    data.addAddress(tokenReceipt, 32,true)
+    data.addAddress(feeToken, 32,true)
+    data.addUint16(walletSplitPercentage, 32)
+    data.addUint16(tokenSFeePercentage, 32)
+    data.addUint16(tokenBFeePercentage, 32)
+    data.addBoolean(allOrNone, 32)
 
-  def toTypeData(): EIP712TypeData = {
-    var types = new EIP712Types
-    types += "EIP712Domain" → Seq(
-      EIP712TypeUnit(name = "name", typ = "string"),
-      EIP712TypeUnit(name = "version", typ = "string")
-    )
-    types += "Order" → Seq(
-      EIP712TypeUnit(name = "amountS", typ = "uint"),
-      EIP712TypeUnit(name = "amountB", typ = "uint"),
-      EIP712TypeUnit(name = "feeAmount", typ = "uint"),
-      EIP712TypeUnit(name = "validSince", typ = "uint"),
-      EIP712TypeUnit(name = "validUntil", typ = "uint"),
-      EIP712TypeUnit(name = "owner", typ = "address"),
-      EIP712TypeUnit(name = "tokenS", typ = "address"),
-      EIP712TypeUnit(name = "tokenB", typ = "address"),
-      EIP712TypeUnit(name = "dualAuthAddr", typ = "address"),
-      EIP712TypeUnit(name = "broker", typ = "address"),
-      EIP712TypeUnit(name = "orderInterceptor", typ = "address"),
-      EIP712TypeUnit(name = "wallet", typ = "address"),
-      EIP712TypeUnit(name = "tokenRecipient", typ = "address"),
-      EIP712TypeUnit(name = "feeToken", typ = "address"),
-      EIP712TypeUnit(name = "walletSplitPercentage", typ = "uint16"),
-      EIP712TypeUnit(name = "tokenSFeePercentage", typ = "uint16"),
-      EIP712TypeUnit(name = "tokenBFeePercentage", typ = "uint16"),
-      EIP712TypeUnit(name = "allOrNone", typ = "bool")
-    )
-    val domain = EIP712Domain(name = "Loopring Protocol", version = "2")
+    val message = Numeric.toHexString(web3Hash.sha3(data.getBytes))
 
-    var message = new EIP712Data
-    message += "amountS" → amountS
-    message += "amountB" → amountB
-    message += "feeAmount" → feeAmount
-    message += "validSince" → BigInt(validSince)
-    message += "validUntil" → BigInt(validUntil)
-    message += "owner" → owner
-    message += "tokenS" → tokenS
-    message += "tokenB" → tokenB
-    message += "dualAuthAddr" → dualAuthAddress
-    message += "broker" → broker
-    message += "orderInterceptor" → orderInterceptor
-    message += "wallet" → wallet
-    message += "tokenRecipient" → tokenReceipt
-    message += "feeToken" → feeToken
-    message += "walletSplitPercentage" → walletSplitPercentage
-    message += "tokenSFeePercentage" → tokenSFeePercentage
-    message += "tokenBFeePercentage" → tokenBFeePercentage
-    message += "allOrNone" → allOrNone
+    val eip = ByteStream()
+    eip.addHex(EIP712_HEADER)
+    eip.addHex(EIP712_DOMAIN_HASH)
+    eip.addHex(message)
 
-    EIP712TypeData(
-      types = types,
-      primaryType = "Order",
-      domain = domain,
-      message = message
-    )
+    Numeric.toHexString(web3Hash.sha3(eip.getBytes))
   }
 }
